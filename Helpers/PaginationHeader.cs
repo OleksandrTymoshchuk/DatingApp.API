a9e4bd66896ceef66bd025f1d4a0442f5d087df7
Oleksandr Tymoshchuk <oleksandrtymoshchuk22@gmail.com>
2020-01-12 23:46:41 +0200

    Rename property in PaginationHeader class

--- Helpers/PaginationHeader.cs
+++ Helpers/PaginationHeader.cs
@@ -4,13 +4,13 @@ namespace DatingApp.API.Helpers
     {
         public int CurrentPage { get; set; }
         public int ItemsPerPage { get; set; }
-        public int TotaItems { get; set; }
+        public int TotalItems { get; set; }
         public int TotalPages { get; set; }
         public PaginationHeader(int currentPage, int itemsPerPage, int totalItems, int totalPages)
         {
             this.CurrentPage = currentPage;
             this.ItemsPerPage = itemsPerPage;
-            this.TotaItems = totalItems;
+            this.TotalItems = totalItems;
             this.TotalPages = totalPages;
         }
     }
